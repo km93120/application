c2e24a5c64a9fcac82a2f886f466dffbe0794818
km93120 <35903578+km93120@users.noreply.github.com>
2018-04-12 08:41:47 +0200

    Create Calcul.java

---
+++ Calcul.java
@@ -0,0 +1,13 @@
+package e.sofian.myapplication;
+
+/**
+ * Created by sofian on 11/04/2018.
+ */
+
+public interface Calcul {
+
+     double distance(double userLat,double userLong,double agenceLat,double agenceLong);
+
+
+
+}
