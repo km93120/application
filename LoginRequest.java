4c8d9833dc65c068021822c0e67e07ab1832ad36
Alou92 <37911570+Alou92@users.noreply.github.com>
2018-05-20 12:21:21 +0000

    Update LoginRequest.java

--- LoginRequest.java
+++ LoginRequest.java
@@ -6,14 +6,16 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginRequest extends StringRequest { // hérite de stringRequest , classe de la bibliotheque Volley, necessaire a l'utilsiation de l'architecture REST
+public class LoginRequest extends StringRequest { // hérite de stringRequest , classe de la bibliotheque Volley,
+                                                  // necessaire a l'utilsiation de l'architecture REST
 
     private static final String LOGIN_REQUEST_URL = "http://madadiatman.000webhostapp.com/login.php"; // l'url qui mène au fichier  PHP distant
     private Map<String,String> parametres;
 
     public LoginRequest (String username,String password, Response.Listener<String> listener)
     {
-        super(Method.POST,LOGIN_REQUEST_URL,listener,null);      // on utilise un conteneur de type clé - valeur pour stocker les informations renseignées dans formulaire
+        super(Method.POST,LOGIN_REQUEST_URL,listener,null);      // on utilise un conteneur de type clé - valeur
+                                                                 // pour stocker les informations renseignées dans formulaire
         parametres = new HashMap<>();
         parametres.put("username",username);        // arguments passés sous leur nom ds la base de donnees, ne fonctionne pas
         parametres.put("password",password);
